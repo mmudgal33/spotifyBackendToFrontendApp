c915f21c94c4a24a264d520531be918dd6d614cd
mmudgal33 <mmudgal33@gmail.com>
2025-10-27 21:13:08 +0530

    commit small changes app

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,5 +1,5 @@
 const production = {
-    url: 'https://spotifybackendtofrontendapp-1.onrender.com//callback',
+    url: 'https://spotifybackendtofrontendapp-1.onrender.com/callback',
     api: 'https://spotifybackendtofrontendapp.onrender.com'
     //api: 'https://mern-task-app-foodorderingfrontend1-api.onrender.com'
   };
